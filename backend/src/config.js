d4a2537674a28b08c0a27dad1fa125406d9de631
Bickeringmonkey <smeadows110280@gmail.com>
2022-09-14 13:31:38 +0100

    updated files

--- backend/src/config.js
+++ backend/src/config.js
@@ -65,7 +65,7 @@ const ROYALTY_SHARE = 7500; // Percentage of the token price that goes to the ro
 const ROYALTY_ADDRESS = "0xE6e93bf0f2Cd072D8855a27495B69ec987455A45"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
 const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the prereveal token uri
-const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
+const PRESALE_WHITELISTED_ADDRESSES = ["0xE6e93bf0f2Cd072D8855a27495B69ec987455A45"]; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
 let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
