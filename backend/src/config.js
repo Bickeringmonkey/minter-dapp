d5ed335db3da54d5745d8c156b9b99daee362c56
Bickeringmonkey <smeadows110280@gmail.com>
2022-09-14 14:51:48 +0100

    updated files

--- backend/src/config.js
+++ backend/src/config.js
@@ -60,7 +60,7 @@ const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a singl
 const PUBLIC_MINT_START_DATE = "2022-09-13T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = "2022-09-12T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 7500; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0xE6e93bf0f2Cd072D8855a27495B69ec987455A45"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
